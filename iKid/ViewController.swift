14af474fc44634b2ba0214ef5364f0a3c26ee382
angsoenW <91903037+angsoenW@users.noreply.github.com>
2024-04-28 15:21:56 -0700

    Initial Commit

---
+++ iKid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iKid
+//
+//  Created by Jasper Wang on 4/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
